74e0e744b06bcacfd6b2de411387ae8f33642863
Charly <carlosr.gomez@udem.edu>
2026-05-22 19:09:37 -0600

    cloudinary photos working

---
+++ api/photos.js
@@ -0,0 +1,47 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+export default async function handler(req, res) {
+
+    try {
+
+        // GET
+        if (req.method === "GET") {
+
+            const result = await cloudinary.search
+                .expression('folder:9meses')
+                .sort_by('created_at','desc')
+                .max_results(100)
+                .execute();
+
+            return res.status(200).json(result.resources);
+        }
+
+        // POST
+        if (req.method === "POST") {
+
+            const { image } = req.body;
+
+            const upload = await cloudinary.uploader.upload(image, {
+                folder: "9meses"
+            });
+
+            return res.status(200).json(upload);
+        }
+
+        return res.status(405).json({
+            error: "Method not allowed"
+        });
+
+    } catch (error) {
+
+        return res.status(500).json({
+            error: error.message
+        });
+    }
+}
